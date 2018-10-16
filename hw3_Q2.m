3b1bba4f6ebb142f45dbacfe51a71f1e324a7c39
Anindita Maiti <aninditamaiti1992@gmail.com>
2018-10-15 22:17:48 -0400

    question 2 corrected

--- hw3_Q2.m
+++ hw3_Q2.m
@@ -17,56 +17,61 @@ m = 1;
 t1 = y1(:,1);
 r1 = y1(:,2);  % theta
 r1dot = y1(:,3); % d(theta)/dt
-E1_total = 0.5*m*R^2*(r1dot.^2) + 0.5*m*R^2*( w2*r1.^2);
+E1_total = 0.5*m*R^2*(r1dot.^2) + m*g*R.*(1 - cos(r1));
 
 [T2, y2] = pendulum1(R,theta0_2, dtheta0, 1);
 t2 = y2(:,1);
 r2 = y2(:,2);  % theta
 r2dot = y2(:,3); % d(theta)/dt
-E2_total = 0.5*m*R^2*(r2dot.^2) + 0.5*m*R^2*( w2*r2.^2);
+E2_total = 0.5*m*R^2*(r2dot.^2) + m*g*R.*(1 - cos(r2));
 
 [T3, y3] = pendulum1(R,theta0_3, dtheta0, 1);
 t3 = y3(:,1);
 r3 = y3(:,2);  % theta
 r3dot = y3(:,3); % d(theta)/dt
-E3_total = 0.5*m*R^2*(r3dot.^2) + 0.5*m*R^2*( w2*r3.^2);
+E3_total = 0.5*m*R^2*(r3dot.^2) + m*g*R.*(1 - cos(r3));
 
 [T4, y4] = pendulum1(R,theta0_4, dtheta0, 1);
 t4 = y4(:,1);
 r4 = y4(:,2);  % theta
 r4dot = y4(:,3); % d(theta)/dt
-E4_total = 0.5*m*R^2*(r4dot.^2) + 0.5*m*R^2*( w2*r4.^2);
+E4_total = 0.5*m*R^2*(r4dot.^2) + m*g*R.*(1 - cos(r4));
 
 [T5, y5] = pendulum1(R,theta0_5, dtheta0, 1);
 t5 = y5(:,1);
 r5 = y5(:,2);  % theta
 r5dot = y5(:,3); % d(theta)/dt
-E5_total = 0.5*m*R^2*(r5dot.^2) + 0.5*m*R^2*( w2*r5.^2);
+E5_total = 0.5*m*R^2*(r5dot.^2) + m*g*R.*(1 - cos(r5));
 
 figure(1);
 plot(t1,r1,'r-',t1,r1dot,'b-',t1,E1_total, 'g-');
-legend('position for theta0=0.1','velocity for theta0=0.1','Total energy');
-title('Theta_0 = 0.1')
+legend('position for \theta_0=0.1','velocity for \theta_0=0.1','Total energy');
+xlabel('t')
+title('\theta_0 = 0.1')
 
 figure(2);
 plot(t2,r2,'r-',t2,r2dot,'b-',t2,E2_total, 'g-');
-legend('position for theta0=0.2','velocity for theta0=0.2','Total energy');
-title('Theta_0 = 0.2')
+legend('position for \theta_0=0.2','velocity for \theta_0=0.2','Total energy');
+xlabel('t')
+title('\theta_0 = 0.2')
 
 figure(3);
 plot(t3,r3,'r-',t3,r3dot,'b-',t3,E3_total, 'g-');
-legend('position for theta0=0.4','velocity for theta0=0.4','Total energy');
-title('Theta_0 = 0.4')
+legend('position for \theta_0=0.4','velocity for \theta_0=0.4','Total energy');
+xlabel('t')
+title('\theta_0 = 0.4')
 
 figure(4);
 plot(t4,r4,'r-',t4,r4dot,'b-',t4,E4_total, 'g-');
-legend('position for theta0=0.8','velocity for theta0=0.8','Total energy');
-title('Theta_0 = 0.8')
+legend('position for \theta_0=0.8','velocity for \theta_0=0.8','Total energy');
+xlabel('t')
+title('\theta_0 = 0.8')
 
 figure(5);
 plot(t5,r5,'r-',t5,r5dot,'b-',t5,E5_total, 'g-');
-legend('position for theta0=1','velocity for theta0=1','Total energy');
-title('Theta_0 = 1')
+legend('position for \theta_0=1','velocity for \theta_0=1','Total energy');
+xlabel('t')
+title('\theta_0 = 1')
 
 sprintf('For each case both theta and d(theta)/dt are sinosoidal in nature and they differ by a phase difference of pi/2')
 sprintf('For nonlinear theta0 = %0.1f period is %0.3f and maximum amplitude is %0.3f',theta0_1,T1,max(r1))
@@ -113,7 +118,9 @@ amp_p_array = [max(r1p), max(r2p), max(r3p), max(r4p), max(r5p)];
 figure(6);
 plot(amp_array,T_array, 'c-', amp_p_array, Tp_array, 'b-');
 legend('nonlinear case', 'linear case');
-title('T vs. Amplitude');
+title('T vs. \theta_{max}');
+xlabel('\theta_{max}')
+ylabel('T')
 
 sprintf('For linear theta0 = %0.1f period is %0.3f and maximum amplitude is %0.3f',theta0_1,T1p,max(r1p))
 sprintf('For linear theta0 = %0.1f period is %0.3f and maximum amplitude is %0.3f',theta0_2,T2p,max(r2p))
